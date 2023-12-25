c581c29adf96c2d2f324b9cfd4cc31cef084a35d
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2023-12-25 14:22:35 +0100

    navigation layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
 "use client";
 
-import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import {ThemeProvider} from "@/components/theme-provider"
+import {Navigation} from "@/components/ui/Navigation";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({children}: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
-        </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+    <body className={inter.className}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className={"flex flex-row"}>
+        <Navigation/>
+        <div className={"p-6"}>
+          {children}
+        </div>
+      </div>
+
+    </ThemeProvider>
+    </body>
+    </html>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import {Home as HomeIcon} from 'lucide-react';
-import {NavigationLinks} from '@/data/navigation-links';
-import Link from 'next/link';
-
 export default function Home() {
-    return (
-        <div className={"w-1/6 h-screen border-r border-white border-1 p-6"}>
-            <h1>TabTerra</h1>
-            <ul className={"text-white pt-20"}>
-                {NavigationLinks.map((navigationElement) => {
-                    return (
-                        <Link key={navigationElement.slug} href={navigationElement.href} className={"flex mt-6 hover:bg-gray-700 p-2 ease-linear duration-100"}>
-                            <HomeIcon />
-                            <li className={"ml-4"}>{navigationElement.name}</li>
-                        </Link>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <p>Test</p>
+  )
 }
