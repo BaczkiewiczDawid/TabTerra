e12a82571586b21f8aca2ec8a35a54c5557e3ccf
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2023-12-25 13:52:28 +0100

    navigation design

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import {Home as HomeIcon} from 'lucide-react';
 import {NavigationLinks} from '@/data/navigation-links';
 import Link from 'next/link';
-import {element} from "prop-types";
 
 export default function Home() {
     return (
-        <div className={"w-1/6 h-screen bg-white"}>
-            <ul className={"text-black pt-20 pl-6"}>
+        <div className={"w-1/6 h-screen border-r border-white border-1 p-6"}>
+            <h1>TabTerra</h1>
+            <ul className={"text-white pt-20"}>
                 {NavigationLinks.map((navigationElement) => {
                     return (
-                        <Link href={navigationElement.href} className={"flex"}>
+                        <Link key={navigationElement.slug} href={navigationElement.href} className={"flex mt-6 hover:bg-gray-700 p-2 ease-linear duration-100"}>
                             <HomeIcon />
                             <li className={"ml-4"}>{navigationElement.name}</li>
                         </Link>
